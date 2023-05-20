a221dbb3828d2dad56ce4039b52e00978c705a89
JaeSeongHong <hjs1144@gmail.com>
2023-05-20 11:49:47 +0900

    docs : 프로그래머스 연습

---
+++ Programers/src/Ex01배열두배만들기.java
@@ -0,0 +1,16 @@
+
+public class Ex01배열두배만들기 {
+
+	public static void main(String[] args) {
+		//numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+		
+		int[] numbers = {1,2,3,4,5};
+		
+		for(int i=0;i<numbers.length;i++) {
+			System.out.print(numbers[i]*2+" ");
+		}
+		
+		
+	}
+
+}
---
+++ Programers/src/Ex02배열원소의길이.java
@@ -0,0 +1,17 @@
+
+public class Ex02배열원소의길이 {
+
+	public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        
+        int length = strlist.length;
+        for(int i = 0; i<length; i++){
+            answer[i] = strlist[i].length();
+        }
+        
+        //map 활용 가능하다.
+        return answer;
+    }
+
+
+}
