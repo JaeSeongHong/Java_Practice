9326a04da70bcab15c4638a27b3205acdbd1b389
JaeSeongHong <hjs1144@gmail.com>
2023-05-22 20:30:15 +0900

    docs : 프로그래머스

--- Programers/src/Ex04피자나눠먹기.java
+++ Programers/src/Ex04피자나눠먹기.java
@@ -1,4 +1,17 @@
 
 public class Ex04피자나눠먹기 {
+	public int solution(int n) {
+        int answer = 0;
+
+        if (n >= 1 && n <= 100) {
+            if (n % 7 == 0) {
+                answer = n / 7; 
+            }
+            else {
+                answer = n / 7 + 1;
+            }
+        }
+        return answer;
+    }
 
 }
